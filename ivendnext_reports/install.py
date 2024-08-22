e0a340b5b528406e36f8490853741dfc282b7c62
Abdul Hannan Shaikh <sahannan96@gmail.com>
2024-08-22 07:45:15 +0000

    fix: Fixed Node Path

--- ivendnext_reports/install.py
+++ ivendnext_reports/install.py
@@ -4,11 +4,14 @@ import os
 
 def after_install():
     app_path = os.path.dirname(os.path.abspath(__file__))  # Get the app directory path
-    node_modules_path = os.path.join(app_path, "NodejsDataAdapters", "node_modules")
+    nodejs_dir = os.path.join(app_path, "NodejsDataAdapters")
+    node_modules_path = os.path.join(nodejs_dir, "node_modules")
 
     if not os.path.exists(node_modules_path):  # Check if node_modules doesn't exist
         try:
-            subprocess.check_call(["npm", "install"], cwd=app_path)
+            subprocess.check_call(
+                ["npm", "install"], cwd=nodejs_dir
+            )  # Change cwd to NodejsDataAdapters
             print("npm install completed successfully.")
         except subprocess.CalledProcessError as e:
             print(f"npm install failed with error: {e}")
